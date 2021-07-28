8a2932717859cfa09503364e41d42021f6c44445
CapnOdin <CapnOdin@gmail.com>
2021-07-28 11:57:52 +0200

    removed old functions and changed it to save plots larger and as pdf in the dir with the data file

--- plotter.py
+++ plotter.py
@@ -1,82 +1,62 @@
-import sys
+import sys, ntpath
 import numpy as np
 import matplotlib.pyplot as plt
 
+#np.set_printoptions(threshold=sys.maxsize)
 
 def moving_average(x, N, fill=True):
-	return np.concatenate([x for x in [ [None]*(N // 2 + N % 2)*fill, np.convolve(x, np.ones((N,))/N, mode='valid'), [None]*(N // 2)*fill, ] if len(x)]) 
+	averaged = np.concatenate([x for x in [ [None]*(N // 2 + N % 2)*fill, np.convolve(x, np.ones((N,))/N, mode='valid'), [None]*(N // 2)*fill, ] if len(x)])
+	#print(f"a len1: {len(averaged)}, len2: {len(averaged[(N // 2 + N % 2):-(N // 2)])}")
+	return averaged[(N // 2 + N % 2):-(N // 2)], (N // 2 + N % 2), (N // 2)
 
-def generateGraphs(paths):
+def getDPI():
+	import tkinter
+	root = tkinter.Tk()
+	dpi = root.winfo_fpixels('1i')
+	root.destroy()
+	return dpi
+
+def getAvalableFilename(path, ext):
+	file = f"{path}.{ext}"
+	if(ntpath.exists(file)):
+		i = 1
+		while(ntpath.exists(f"{path} ({i}).{ext}")):
+			i += 1
+		file = f"{path} ({i}).{ext}"
+	return file
+
+def GenerateGraphs(paths):
 	plots = []
-	fig, ax = plt.subplots()
+	dpi = getDPI()
+	#fig, ax = plt.subplots(figsize=(1920/dpi, 1080/dpi), dpi=dpi)
+	fig, ax = plt.subplots(figsize=(3840/dpi, 2160/dpi), dpi=dpi)
 	ax.yaxis.grid()
+	headers = []
 	for path in paths:
-		csv = open(path, "r").readlines()
-		sf, cr, bw = csv[0].replace("\n", "").split(",")
-		x, y = zip(*[list(map(float, line.split(","))) for line in csv[1:]])
-		plots.append(ax.plot(x, y, '.-', label=bw))
-	
-	#ax.legend(handles=plots)
-	ax.legend()
-	ax.set(xlabel='msg lenght', ylabel='time (ms)',
-	       title=f'Tx delay over msg size - {sf} - {cr}')
-	fig.savefig(f"LoRa tx time - {sf} - {cr}.png")
-	plt.show()
-
-def generateGraph(path):
-	csv = open(path, "r").readlines()
-	sf, cr, bw = csv[0].replace("\n", "").split(",")
-	x, y = zip(*[list(map(int, line.split(","))) for line in csv[1:]])
-	
-	fig, ax = plt.subplots()
-	ax.plot(x, y, '.-')
-	ax.set(xlabel='msg lenght', ylabel='time (ms)',
-	       title=f'Tx delay over msg size - {sf} - {cr} - {bw}')
-	fig.savefig(f"LoRa tx time - {sf} - {bw}.png")
-	plt.show()
-	
-def generateGraph2(path):
-	plots = []
-	fig, ax = plt.subplots()
-	ax.yaxis.grid()
-	csv = [line.split(",") for line in open(path, "r").read().split("\n")]
-	headers = csv[0]
-	for i, head in enumerate(headers[1:]):
-		x, y = zip(*[list(map(float, (line[0], line[i+1]))) for line in csv[1:]])
-		plots.append(ax.plot(x, y, '.-', label=head))
+		name = ntpath.basename(path)
+		csv = [line.split(",") for line in open(path, "r").read().split("\n")]
+		headers = csv[0]
+		x, y = zip(*[list(map(float, (line[0], line[1]))) for line in csv[1:] if line[0] != "" and line[1] != ""])
+		if(len(headers) == 3):
+			plots.append(ax.plot(x, y, '.-', label = name)) #, linewidth = 1, markersize = 2
+		elif(len(headers) > 3):
+			if(headers[3] == "average"):
+				averagedY, offsetStart, offsetEnd = moving_average(y, 5)
+				#print(f"x len1: {len(x)}, len2: {len(x[offsetStart:-(offsetEnd)])}")
+				#print(f"y len1: {len(y)}, len2: {len(y[offsetStart:-(offsetEnd)])}")
+				plots.append(ax.plot(x, y, '.', label = f"{name}"))
+				plots.append(ax.plot(x[offsetStart - 1:-offsetEnd], averagedY, '-', label = f"averaged-{name}"))
 	
 	ax.legend()
-	ax.set(xlabel='msg lenght', ylabel='time (ms)',
-	       title=f'Tx delay over msg size')
-	fig.savefig(f"LoRa tx time.png")
-	plt.show()
-	
-def generateGraph3(path):
-	plots = []
-	fig, ax = plt.subplots()
-	ax.yaxis.grid()
-	csv = [line.split(",") for line in open(path, "r").read().split("\n")]
-	headers = csv[0]
-	for i, head in enumerate(headers[1:]):
-		x, y = zip(*[list(map(float, (line[0], line[i+1]))) for line in csv[1:]])
-		#npy = np.array(y)
-		N = len(y)
-		npy = moving_average(y, 5)
-		#npy = np.convolve(y, np.ones((N,))/N, mode='valid')
-		print(npy)
-		plots.append(ax.plot(x, npy[0:100], '.-', label=head))
-	
-	ax.legend()
-	ax.set(xlabel='msg lenght', ylabel='time (ms)',
-	       title=f'Tx delay over msg size')
-	fig.savefig(f"LoRa tx time.png")
+	ax.set(xlabel=headers[1], ylabel=headers[2], title=headers[0])
+	fig.savefig(getAvalableFilename(paths[0], "pdf"))
 	plt.show()
 
 def main():
 	if(len(sys.argv) > 2):
-		generateGraphs(sys.argv[1:])
+		GenerateGraphs(sys.argv[1:])
 	else:
-		generateGraph2(sys.argv[1])
+		GenerateGraphs([sys.argv[1]])
 
 if __name__ == "__main__":
 	main()
