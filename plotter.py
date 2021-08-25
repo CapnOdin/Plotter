dccce1ca7afd6f178c9b3f04f2e464eac2e5f49d
CapnOdin <CapnOdin@gmail.com>
2021-08-25 16:54:25 +0200

    made it so that annotations do not change the size of the plot if there are more than one annotation file

--- plotter.py
+++ plotter.py
@@ -34,23 +34,37 @@ def GenerateGraphs(paths):
 	title = []
 	xlabel = []
 	ylabel = []
+	
+	annotations = [path for path in paths if "annotate" in open(path, "r").readline()]
+	paths = [path for path in paths if "annotate" not in open(path, "r").readline()]
+	
 	for path in paths:
 		name = ntpath.basename(path)
 		csv = [line.split(",") for line in open(path, "r").read().split("\n")]
 		headers = csv[0]
 		graphs = parseGraphs(headers, csv[1:], name)
-		if(graphs["title"] not in title):
+		if(graphs["title"] and graphs["title"] not in title):
 			title.append(graphs["title"])
-		if(graphs["xlabel"] not in xlabel):
+		if(graphs["xlabel"] and graphs["xlabel"] not in xlabel):
 			xlabel.append(graphs["xlabel"])
-		if(graphs["ylabel"] not in ylabel):
+		if(graphs["ylabel"] and graphs["ylabel"] not in ylabel):
 			ylabel.append(graphs["ylabel"])
 	
 		for graph in graphs["graphs"]:
 			x = graph["x"]
 			y = graph["y"]
 			label = graph["label"]
-			if("average" in graphs["options"]):
+			if("vline" in graphs["options"]):
+				ymin, ymax = ax.get_ylim()
+				plots.append(ax.vlines(x, ymin, ymax, label = label))
+				#for i, xval in enumerate(x):
+					#ax.axvline(x = xval)
+					#ax.annotate(y[i], (float(xval), float(ymin)), xytext = (float(xval), float(ymin) - (float(ymax) * 0.05)), horizontalalignment = 'right', verticalalignment = "top", rotation = 40, arrowprops={"arrowstyle": "->", "relpos": (1, 1)}, annotation_clip = False)
+			#elif("annotate" in graphs["options"]):
+			#	ymin, ymax = ax.get_ylim()
+			#	for i, xval in enumerate(x):
+			#		ax.annotate(y[i], (float(xval), float(ymin)), xytext = (float(xval), float(ymin) - (float(ymax) * 0.05)), horizontalalignment = 'right', verticalalignment = "top", rotation = 40, arrowprops={"arrowstyle": "->", "relpos": (1, 1)}, annotation_clip = False)
+			elif("average" in graphs["options"]):
 				averagedY, offsetStart, offsetEnd = moving_average(y, 5)
 				#print(f"x len1: {len(x)}, len2: {len(x[offsetStart:-(offsetEnd)])}")
 				#print(f"y len1: {len(y)}, len2: {len(y[offsetStart:-(offsetEnd)])}")
@@ -58,39 +72,68 @@ def GenerateGraphs(paths):
 				plots.append(ax.plot(x[offsetStart - 1:-offsetEnd], averagedY, '-', label = f"averaged-{label}"))
 			else:
 				plots.append(ax.plot(x, y, '.-', label = label))
+	
+	ymin, ymax = ax.get_ylim()
+	for path in annotations:
+		name = ntpath.basename(path)
+		csv = [line.split(",") for line in open(path, "r").read().split("\n")]
+		headers = csv[0]
+		graphs = parseGraphs(headers, csv[1:], name)
+		if(graphs["title"] and graphs["title"] not in title):
+			title.append(graphs["title"])
+		if(graphs["xlabel"] and graphs["xlabel"] not in xlabel):
+			xlabel.append(graphs["xlabel"])
+		if(graphs["ylabel"] and graphs["ylabel"] not in ylabel):
+			ylabel.append(graphs["ylabel"])
 		
+		for graph in graphs["graphs"]:
+			x = graph["x"]
+			y = graph["y"]
+			label = graph["label"]
+			colour = next(ax._get_lines.prop_cycler)['color']
+			for i, xval in enumerate(x):
+				ax.annotate(y[i], (float(xval), float(ymin)), xytext = (0, -50), horizontalalignment = 'right', verticalalignment = "top", rotation = 40, arrowprops={"arrowstyle": "->", "relpos": (1, 1), "alpha": 0.5, "color": colour}, annotation_clip = False, xycoords = "data", textcoords = "offset points")
 	
 	ax.legend()
 	ax.set(xlabel = "|".join(xlabel), ylabel = " | ".join(ylabel), title = " | ".join(title))
 	fig.savefig(getAvalableFilename(paths[0], "pdf"))
 	plt.show()
 
-Options = ["average", "generateX"]
+Options = ["average", "generateX", "vline", "annotate"]
 
 def parseGraphs(header, rows, filename = ""):
 	graphs = {"graphs": [], "title": header[0], "xlabel": header[1], "ylabel": header[2], "options": []}
 	
 	num_columns = len(rows[0])
-	
 	i = 0
+	x = []
+	
 	labels = [label for label in header[3:] if label not in Options]
 	graphs["options"].extend([option for option in header[3:] if option in Options])
-	x = []
+	
+	vline = "vline" in graphs["options"]
+	annotate = "annotate" in graphs["options"]
+	generateX = ("generateX" in header[3:]) or num_columns == 1
 	
 	if(not len(labels)):
 		labels.append(filename)
 	
-	generateX = ("generateX" in header[3:]) or num_columns == 1
 	if(not generateX):
 		x = [float(column[0]) for column in rows]
 		i = 1
 	
 	for label in labels:
-		y = [float(column[i]) for column in rows]
-		if(generateX):
-			x = range(len(y))
-		graphs["graphs"].append({"x": x, "y": y, "label": label})
-		i += 1
+		if(vline or annotate):
+			x = [float(column[0]) for column in rows]
+			y = [column[1] for column in rows]
+			graphs["graphs"].append({"x": x, "y": y, "label": label})
+			break
+		else:
+			y = [float(column[i]) for column in rows]
+			if(generateX):
+				x = range(len(y))
+			graphs["graphs"].append({"x": x, "y": y, "label": label})
+			i += 1
 	return graphs
 
 def main():
